8a6bf293d5a7202cf18b5d914e6424333cabeca4
David Gageot <david.gageot@docker.com>
2023-12-03 08:07:35 +0100

    day 03
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 03/first.rb
+++ 03/first.rb
@@ -1,7 +1,27 @@
 #!/usr/bin/env ruby
-# Expected: N/A
+# Expected: 4361
 
-# require "scanf"
+grid = STDIN.readlines(chomp: true).map { |line| line.chars }
 
-p STDIN
-    .readlines()
+def symbol?(grid, y, x)
+    !(grid[y].nil? || grid[y][x].nil? || /[\.\d]/.match(grid[y][x]))
+end
+
+def near_symbol?(grid, group, y, x)
+    (y-1..y+1).any? { |yy| (x-1..x+group.length).any? { |xx| symbol?(grid, yy, xx) } }
+end
+
+def find_numbers(grid)
+    grid.each.with_index do |row, y|
+        x = 0
+        groups = row.join.split(/(\d+)/)
+        groups.each do |group|
+            yield group.to_i if /\d+/.match(group) && near_symbol?(grid, group, y, x)
+            x += group.length
+        end
+    end
+end
+
+sum = 0
+find_numbers(grid) { |n| sum += n }
+p sum
--- 03/second.rb
+++ 03/second.rb
@@ -1,7 +1,30 @@
 #!/usr/bin/env ruby
-# Expected: N/A
+# Expected: 467835
 
-# require "scanf"
+grid = STDIN.readlines(chomp: true).map { |line| line.chars }
 
-p STDIN
-    .readlines()
+def gear?(grid, y, x)
+    (grid[y] || [])[x] == "*"
+end
+
+def search_gears(grid)
+    grid.each.with_index do |row, y|
+        x = 0
+        groups = row.join.split(/(\d+)/)
+        groups.each do |group|
+            if /\d+/.match(group)
+                (y-1..y+1).each do |yy|
+                    (x-1..x+group.length).each do |xx|
+                        yield [[yy, xx], group.to_i] if gear?(grid, yy, xx)
+                    end
+                end
+            end
+
+            x += group.length
+        end
+    end
+end
+
+found = []
+search_gears(grid) { |gear| found << gear }
+p found.group_by(&:shift).values.select { |v| v.length == 2}.map(&:flatten).sum { |pair| pair[0] * pair[1] }
